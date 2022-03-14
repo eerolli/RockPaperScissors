f2558630b1daaab634b345c791cd43dc24123b95
eerolli <eero_liuska_8@hotmail.com>
2022-03-15 00:00:20 +0200

    Added playRound function

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
 let choices = ["rock", "paper", "scissors"]
+let playerChoice = prompt("Please type either 'rock', 'paper' or 'scissors' to play: ")
+let playerSelection = playerChoice.toLowerCase();
 let computerSelection = computerPlay();
-let playerSelection = prompt("Play a round of rock paper scissors by typing either 'rock', 'paper' or 'scissors': ")
 
 function computerPlay(){
     //computer chooses one of the three choices at random
-    let selection = choices[Math.floor(Math.random() * choices.length)];
-    return selection;
+    let random = choices[Math.floor(Math.random() * choices.length)];
+    return random;
 }
 
-function round(computerSelection, playerSelection) {
+function playRound(computerSelection, playerSelection) {
+ 
+    if (playerSelection === computerSelection) {
+        return("It is a tie! Try again!");
+    }
+
+    if (computerSelection === "rock" && playerSelection === "paper") {
+        return("You win! Paper beats rock.");
+    }
+
+    if (computerSelection === "scissors" && playerSelection === "rock") {
+        return("You win! Rock beats scissors.");
+    }
+
+    if (computerSelection === "paper" && playerSelection === "scissors") {
+        return("You win! Scissors beat paper.");
+    }
+
+    if (computerSelection === "rock" && playerSelection === "scissors") {
+        return("You lose! Rock beats scissors.");
+    }
     
+    if (computerSelection === "paper" && playerSelection === "rock") {
+        return("You lose! Paper beats rock.");
+    }
+
+    if (computerSelection === "scissors" && playerSelection === "paper") {
+        return("You lose! Scissors beat paper.");
+    }
 }
 
-console.log("Computer chose " + computerPlay());
+console.log(playRound(computerSelection, playerSelection));
