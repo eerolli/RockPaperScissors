ee02c740e6198d8ae011878b15553461a7fbb0d8
eerolli <eero_liuska_8@hotmail.com>
2022-03-17 09:46:28 +0200

    Added function that loops the game five times. The result is always a tie

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # RockPaperScissors
-The Odin Project challenge
+# The Odin Project challenge
+# A console based rock paper scissors game
--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let choices = ["rock", "paper", "scissors"]
-let playerChoice = prompt("Please type either 'rock', 'paper' or 'scissors' to play: ")
-let playerSelection = playerChoice.toLowerCase();
+let userScore = 0;
+let computerScore = 0;
+let playerSelection = playerPlay();
 let computerSelection = computerPlay();
+
+function playerPlay(){
+    //player input
+    let playerChoice = prompt("Please type either 'rock', 'paper' or 'scissors' to play: ").toLowerCase();
+    return playerChoice;
+}
 
 function computerPlay(){
     //computer chooses one of the three choices at random
@@ -9,35 +16,53 @@ function computerPlay(){
     return random;
 }
 
-function playRound(computerSelection, playerSelection) {
- 
+function playRound(playerSelection, computerSelection) {
+    //one round of the game
+
+    computerPlay();
+    playerPlay();
+    
     if (playerSelection === computerSelection) {
-        return("It is a tie! Try again!");
+        console.log("It is a tie!");
     }
 
     if (computerSelection === "rock" && playerSelection === "paper") {
-        return("You win! Paper beats rock.");
+        userScore++;
+        return("You win! Paper beats rock");
     }
 
     if (computerSelection === "scissors" && playerSelection === "rock") {
-        return("You win! Rock beats scissors.");
+        userScore++;
+        return("You win! Rock beats scissors");
     }
 
     if (computerSelection === "paper" && playerSelection === "scissors") {
-        return("You win! Scissors beat paper.");
+        userScore++;
+        return("You win! Scissors beats paper");
     }
 
     if (computerSelection === "rock" && playerSelection === "scissors") {
-        return("You lose! Rock beats scissors.");
+        computerScore++;
+        return("You lose! Rock beats scissors");
     }
     
     if (computerSelection === "paper" && playerSelection === "rock") {
-        return("You lose! Paper beats rock.");
+        computerScore++;
+        return("You lose! Paper beats rock");
     }
 
     if (computerSelection === "scissors" && playerSelection === "paper") {
-        return("You lose! Scissors beat paper.");
+        computerScore++;
+        return("You lose! Scissors beats paper");
     }
 }
 
-console.log(playRound(computerSelection, playerSelection));
+function game(){
+    //play the game for five rounds
+    for (let i = 0; i < 5; i++) {
+    playRound();        
+    }
+    console.log(`You ${userScore} vs. ${computerScore} Computer`);
+}
+
+game();
